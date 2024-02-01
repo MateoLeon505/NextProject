e9b55e1bce9a24ecfee09b3b25428f9b21740177
mateoleont <mateolt505@gmail.com>
2024-02-01 17:55:25 -0500

    sub-navBar shopping

---
+++ app/shopping/layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+
+const ShoppingLayout = ({children}) => {
+  return (
+    <>
+        <h3>NavBar of shopping</h3>
+        <ul>
+            <li>
+            <Link href="/shopping/categories">Categories</Link>
+            </li>
+            <li>
+            <Link href="/shopping/sellings">Sellings</Link>
+            </li>
+        </ul>
+        {children}
+    </>
+  )
+}
+
+export default ShoppingLayout
--- app/shopping/page.jsx
+++ app/shopping/page.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Link from "next/link";
 
 const Shopping = () => {
 
   return (
     <div>
-      <h1>Shopping</h1>
+      <h1>Shopping Home</h1>
     </div>
   )
 
